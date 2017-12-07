c3bc01b5a0d38295a2bfd2d68debbfa7dd348688
Viking & Robot <vikingandrobot@gmail.com>
2017-12-07 09:48:59 +0100

    Commented LamportApplication.java

--- PRR_labo02_RMI/src/main/java/ch/heigvd/prr_labo02_rmi/lamport/LamportApplication.java
+++ PRR_labo02_RMI/src/main/java/ch/heigvd/prr_labo02_rmi/lamport/LamportApplication.java
@@ -1,3 +1,9 @@
+/**
+ * File: LamportApplication.java
+ * Authors: Sathiya Kirushnapillai & Mathieu Monteverde
+ * Date: 07.12.2017
+ */
+
 package ch.heigvd.prr_labo02_rmi.lamport;
 
 import ch.heigvd.prr_labo02_rmi.lamport.rmi.Lamport;
@@ -10,12 +16,52 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * Main class for the Lamport application.
+ * Main class for the Lamport application program. It creates the Lamport
+ * remote object and registers it to the RMI registry.
+ * 
+ * To start the application you should first start the RMI registry from the 
+ * root class folder using the command 'rmiregistry'.
+ * 
+ * Then you start this application by using the command: 
+ * 
+ * java -jar -Djava.rmi.server.codebase=file:<absolute path to the maven project>/target/classes/ 
+ * target/lamport_application.jar <RMI registry address> <number of Lamport applications> 
+ * <ID of the current application (starting at 0)>
+ * 
+ * This command comes from the official RMI documentation: 
+ * https://docs.oracle.com/javase/7/docs/technotes/guides/rmi/hello/hello-world.html
+ * 
+ * You should start every Lamport appliation to before starting any of the user
+ * application. Because of lack of time and because it wasn't explicitly
+ * asked, the Lamport application only supports the case where one user
+ * application is run per Lamport application.
+ * 
+ * Testing and results:
+ * We used the test command available in the user application to test this 
+ * Lamport application and we concluded that our Lamport implementation
+ * doesn't work correctly. If we execute 10000 tests for example, the resulting
+ * value doesn't equal 20000 but for example 19991. We see that we loose a few 
+ * incrementations in the process and we haven't been able to locate the source
+ * of the problem. 
+ * 
+ * On the other hand the values are shared across the network. We suppose it is 
+ * a problem of mutual exclusion. 
+ * 
  */
 public class LamportApplication {
    
-   private Lamport lamport;
-
+   // The Lamport remote object created
+   private final Lamport lamport;
+   
+   /**
+    * Constructor. Create the remote object and register it to the RMI registry
+    * 
+    * @param rmiAddress the address of the RMI registry
+    * @param numberOfApplications the number of application in use
+    * @param id the id of the current Lamport application
+    * @throws RemoteException
+    * @throws AlreadyBoundException 
+    */
    private LamportApplication(String rmiAddress, int numberOfApplications, int id) 
            throws RemoteException, AlreadyBoundException {
       System.out.println("Starting the Lamport application...");
@@ -27,7 +73,7 @@ public class LamportApplication {
       LocateRegistry.getRegistry().bind("lamport-" + id, lamport);
       System.out.println("Lamport application ready...");
    }
-
+   
    public static void main(String[] args) {
 
       if (args.length != 3) {
@@ -37,7 +83,7 @@ public class LamportApplication {
          System.out.println("Parameters: ");
          System.out.println(" <RMI registry address>");
          System.out.println(" <number of Lamport applications>");
-         System.out.println(" <application ID (integer >=0)>");
+         System.out.println(" <application ID (integer >= 0)>");
          System.exit(0);
       }
 
@@ -52,17 +98,17 @@ public class LamportApplication {
                  + "or the application ID.");
          System.exit(0);
       }
-
+      
+      // Check as much integrity as possible
       if (n <= 0 || id < 0) {
          System.out.println("The number of applications or the ID are not correct.");
          System.exit(0);
       }
 
       try {
+         // Start the application
          LamportApplication application = new LamportApplication(rmiAddress, n, id);
-      } catch (RemoteException ex) {
-         Logger.getLogger(LamportApplication.class.getName()).log(Level.SEVERE, null, ex);
-      } catch (AlreadyBoundException ex) {
+      } catch (RemoteException | AlreadyBoundException ex) {
          Logger.getLogger(LamportApplication.class.getName()).log(Level.SEVERE, null, ex);
       }
    }
